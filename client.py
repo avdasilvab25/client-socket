c8049a698e41c762d94986230d508a5e35035bde
Andrea Valentina Da Silva Baudet <avdasilvab.17@est.ucab.edu.ve>
2020-11-11 14:58:56 -0600

    Modifies the printing format of the message received

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import socket
-import base64
 
 PORT = 19876
 SERVER = '127.0.0.1'
@@ -8,9 +7,17 @@ ADDR = (SERVER, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def send(msg):
-    client.send(b'helloiam avdasilvab.17')
+def send_cmd(msg):
+    encoded_msg = bytes(msg, encoding='utf-8')
+    client.send(encoded_msg)
     data = client.recv(1024)
-    print(data)
+    print('Message received:', data.decode('utf-8'))
 
-send('helloiam avdasilvab.17')
+def request_msg():
+    send_cmd('helloiam avdasilvab.17')
+    send_cmd('msglen')
+    # send_cmd('givememsg')
+    # send_cmd('chkmsg')
+
+# send_cmd('helloiam avdasilvab.17')
+request_msg()
